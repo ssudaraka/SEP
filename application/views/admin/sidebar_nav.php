eaebf5ee726340ef72f574b49511f1214909eca8
Supun Sudaraka <ssudaraka@gmail.com>
2015-03-17 15:17:12 +0530

    Revert "Revert "halfway into password changing""
    This reverts commit 289252cbfa948acfa31c33379e577a6817c834dc.

---
+++ application/views/admin/sidebar_nav.php
@@ -0,0 +1,55 @@
+<div class="panel-group" id="accordion">
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h4 class="panel-title">
+                <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne"><span class="glyphicon glyphicon-folder-close">
+                    </span>Personal Settings</a>
+            </h4>
+        </div>
+        <div id="collapseOne" class="panel-collapse collapse in">
+            <div class="panel-body">
+                <table class="table">
+                    <tr>
+                        <td>
+                            <span class="glyphicon glyphicon-user text-primary"></span><a href="<?php echo base_url('index.php/admin'); ?>">Profile</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <span class="glyphicon glyphicon-cog text-warning"></span><a href="<?php echo base_url('index.php/admin/account_settings'); ?>">Account Settings</a>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h4 class="panel-title">
+                <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo"><span class="glyphicon glyphicon-globe">
+                    </span>Global Settings</a>
+            </h4>
+        </div>
+        <div id="collapseTwo" class="panel-collapse collapse">
+            <div class="panel-body">
+                <table class="table">
+                    <tr>
+                        <td>
+                            <span class="glyphicon glyphicon-wrench text-warning"></span><a href="#">System Settings</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <span class="glyphicon glyphicon-star text-warning"></span><a href="#">Create Admin Account</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <span class="glyphicon glyphicon-list-alt text-warning"></span><a href="#">Manage Administrators</a>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
