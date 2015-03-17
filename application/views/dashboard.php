83e6af86290bf17b7951851f747dc385c995816c
Udara Karunarathna <udarapathmin@gmail.com>
2015-03-17 11:55:12 +0530

    Navbar Margin fix and Dashboard view
    Navbar Margin fix and Dashboard view

---
+++ application/views/dashboard.php
@@ -0,0 +1,50 @@
+<div class="container">
+	<div class="row">
+		<div class="col-md-6">
+			<div class="panel panel-default">
+		        <div class="panel-heading">
+		            <strong>Header 1</strong>
+		        </div>
+		        <div class="panel-body">
+		        </div>
+		    </div>
+		</div>
+	    <div class="col-md-6">
+	    	<div class="panel panel-default">
+	            <div class="panel-heading">
+	                <strong>Header 2</strong>
+	            </div>
+	            <div class="panel-body">
+	            	<p>Test 124</p>
+	            	<p>Test 124</p>
+	            	<p>Test 124</p>
+	            	<p>Test 124</p>
+	            </div>
+	    	</div>
+	    </div>
+    </div>
+    <div class="row">
+		<div class="col-md-6">
+			<div class="panel panel-default">
+		        <div class="panel-heading">
+		            <strong>Header 3</strong>
+		        </div>
+		        <div class="panel-body">
+		        	<p>Test 124</p>
+		        	<p>Test 124</p>
+		        	<p>Test 124</p>
+		        	<p>Test 124</p>
+		        </div>
+		    </div>
+		</div>
+	    <div class="col-md-6">
+	    	<div class="panel panel-default">
+	            <div class="panel-heading">
+	                <strong>Header 4</strong>
+	            </div>
+	            <div class="panel-body">
+	            </div>
+	    	</div>
+	    </div>
+    </div>
+</div>
