5f4c365fa399ae87c1b32472e34abe7b0be0309d
Supun Sudaraka <ssudaraka@gmail.com>
2015-03-17 15:22:54 +0530

    taken new dashboard from udaraka

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -3,36 +3,19 @@
 		<div class="col-md-6">
 			<div class="panel panel-default">
 		        <div class="panel-heading">
-		            <strong><i class="fa fa-ellipsis-h" style="margin-right:10px"></i> NEED YOUR ATTENTION</strong>
+		            <strong>Header 1</strong>
 		        </div>
 		        <div class="panel-body">
-		        	<div class="row" style="padding:20px">
-		        		<center>
-						 <div id="big_stats" class="cf">
-                    <div class="stat"> <i class="fa fa-newspaper-o"></i> <span class="value">851</span> </div>
-                    <!-- .stat -->
-                    
-                    <div class="stat"> <i class="fa fa-newspaper-o"></i> <span class="value">423</span> </div>
-                    <!-- .stat -->
-                    
-                    <div class="stat"> <i class="fa fa-newspaper-o"></i> <span class="value">922</span> </div>
-                    <!-- .stat -->
-                    
-                    <div class="stat"> <i class="fa fa-newspaper-o"></i> <span class="value">25%</span> </div>
-                    <!-- .stat --> 
-                  </div>
-						</center>
-					</div>
 		        </div>
 		    </div>
 		</div>
 	    <div class="col-md-6">
 	    	<div class="panel panel-default">
 	            <div class="panel-heading">
-	                <strong><i class="fa fa-newspaper-o" style="margin-right:10px"></i> RECENT NEWS</strong>
+	                <strong>Header 2</strong>
 	            </div>
 	            <div class="panel-body">
-	            	<p><i class="fa fa-bed"></i></p>
+	            	<p>Test 124</p>
 	            	<p>Test 124</p>
 	            	<p>Test 124</p>
 	            	<p>Test 124</p>
@@ -44,7 +27,7 @@
 		<div class="col-md-6">
 			<div class="panel panel-default">
 		        <div class="panel-heading">
-		            <strong><i class="fa fa-exchange" style="margin-right:10px"></i> ACTIVITY FEED</strong>
+		            <strong>Header 3</strong>
 		        </div>
 		        <div class="panel-body">
 		        	<p>Test 124</p>
@@ -57,7 +40,7 @@
 	    <div class="col-md-6">
 	    	<div class="panel panel-default">
 	            <div class="panel-heading">
-	                <strong><i class="fa fa-calendar" style="margin-right:10px"></i> UPCOMING EVENTS</strong>
+	                <strong>Header 4</strong>
 	            </div>
 	            <div class="panel-body">
 	            </div>
