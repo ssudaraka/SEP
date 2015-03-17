2161df3fe27eccd4d4be125716a11336032e6f63
Supun Sudaraka <ssudaraka@gmail.com>
2015-03-17 15:16:15 +0530

    Revert "dashboard change"
    This reverts commit ebdffe1fe07add5354b33bfb1d1388ff7ec2e8bf.

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -7,29 +7,21 @@
 		        </div>
 		        <div class="panel-body">
 		        	<div class="row" style="padding:20px">
-
-					<div id="big_stats" class="cf">
-                    <div class="stat"> <i class="fa fa-bed"></i> <span class="value">8</span>
-                        <br/>Leaves
-                    </div>
+		        		<center>
+						 <div id="big_stats" class="cf">
+                    <div class="stat"> <i class="fa fa-newspaper-o"></i> <span class="value">851</span> </div>
                     <!-- .stat -->
                     
-                    <div class="stat"> <i class="glyphicon glyphicon-bullhorn"></i> <span class="value">3</span>
-                        <br/>Events
-                    </div>
+                    <div class="stat"> <i class="fa fa-newspaper-o"></i> <span class="value">423</span> </div>
                     <!-- .stat -->
                     
-                    <div class="stat"> <i class="fa fa-newspaper-o"></i> <span class="value">22</span>
-                        <br/>News
-                    </div>
+                    <div class="stat"> <i class="fa fa-newspaper-o"></i> <span class="value">922</span> </div>
                     <!-- .stat -->
                     
-                    <div class="stat"> <i class="fa fa-envelope"></i> <span class="value">25</span>
-                        <br/>Messages
-                    </div>
+                    <div class="stat"> <i class="fa fa-newspaper-o"></i> <span class="value">25%</span> </div>
                     <!-- .stat --> 
                   </div>
-
+						</center>
 					</div>
 		        </div>
 		    </div>
@@ -55,82 +47,10 @@
 		            <strong><i class="fa fa-exchange" style="margin-right:10px"></i> ACTIVITY FEED</strong>
 		        </div>
 		        <div class="panel-body">
-                    <!-- Activity Feed element -->
-                    <div class="row">
-                        <div class="col-sm-2">
-                            <div class="thumbnail">
-                                <img class="img-responsive user-photo" src="https://ssl.gstatic.com/accounts/ui/avatar_2x.png">
-                            </div>
-                        </div><!-- /col-sm-1 -->
-
-                        <div class="col-sm-10">
-                            <div class="panel panel-default">
-                                <div class="panel-heading">
-                                    <strong>myusername</strong> <span class="text-muted">commented 5 days ago</span>
-                                </div>
-                                <div class="panel-body">
-                                    Panel content
-                                </div><!-- /panel-body -->
-                            </div><!-- /panel panel-default -->
-                        </div><!-- /col-sm-5 -->
-                        </div>
-                    <!-- Activity Feed element -->
-                    <div class="row">
-                        <div class="col-sm-2">
-                            <div class="thumbnail">
-                                <img class="img-responsive user-photo" src="https://ssl.gstatic.com/accounts/ui/avatar_2x.png">
-                            </div>
-                        </div><!-- /col-sm-1 -->
-
-                        <div class="col-sm-10">
-                            <div class="panel panel-default">
-                                <div class="panel-heading">
-                                    <strong>myusername</strong> <span class="text-muted">commented 5 days ago</span>
-                                </div>
-                                <div class="panel-body">
-                                    Panel content
-                                </div><!-- /panel-body -->
-                            </div><!-- /panel panel-default -->
-                        </div><!-- /col-sm-5 -->
-                    </div>
-                    <!-- Activity Feed element -->
-                    <div class="row">
-                        <div class="col-sm-2">
-                            <div class="thumbnail">
-                                <img class="img-responsive user-photo" src="https://ssl.gstatic.com/accounts/ui/avatar_2x.png">
-                            </div>
-                        </div><!-- /col-sm-1 -->
-
-                        <div class="col-sm-10">
-                            <div class="panel panel-default">
-                                <div class="panel-heading">
-                                    <strong>myusername</strong> <span class="text-muted">commented 5 days ago</span>
-                                </div>
-                                <div class="panel-body">
-                                    Panel content
-                                </div><!-- /panel-body -->
-                            </div><!-- /panel panel-default -->
-                        </div><!-- /col-sm-5 -->
-                    </div>
-                    <!-- Activity Feed element -->
-                    <div class="row">
-                        <div class="col-sm-2">
-                            <div class="thumbnail">
-                                <img class="img-responsive user-photo" src="https://ssl.gstatic.com/accounts/ui/avatar_2x.png">
-                            </div>
-                        </div><!-- /col-sm-1 -->
-
-                        <div class="col-sm-10">
-                            <div class="panel panel-default">
-                                <div class="panel-heading">
-                                    <strong>myusername</strong> <span class="text-muted">commented 5 days ago</span>
-                                </div>
-                                <div class="panel-body">
-                                    Panel content
-                                </div><!-- /panel-body -->
-                            </div><!-- /panel panel-default -->
-                        </div><!-- /col-sm-5 -->
-                    </div>
+		        	<p>Test 124</p>
+		        	<p>Test 124</p>
+		        	<p>Test 124</p>
+		        	<p>Test 124</p>
 		        </div>
 		    </div>
 		</div>
@@ -140,33 +60,6 @@
 	                <strong><i class="fa fa-calendar" style="margin-right:10px"></i> UPCOMING EVENTS</strong>
 	            </div>
 	            <div class="panel-body">
-                    <ul class="news-items">
-                        <li>
-
-                            <div class="news-item-date"> <span class="news-item-day">29</span> <span class="news-item-month">Aug</span> </div>
-                            <div class="news-item-detail"> <a href="#" class="news-item-title" target="_blank">Event Name #1</a>
-                                <p class="news-item-preview">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra justo atLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra justo at massa suscipit, sit amet vehicula leo tristique. Phasellus vitae tincidunt tortor, at pulvinar augue.</p>
-                            </div>
-
-                        </li>
-                        <li>
-
-                            <div class="news-item-date"> <span class="news-item-day">15</span> <span class="news-item-month">Jun</span> </div>
-                            <div class="news-item-detail"> <a href="#" class="news-item-title" target="_blank">Event Name #1</a>
-                                <p class="news-item-preview">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra justo at massa suscipit, sit amet vehicula leo tristique. Phasellus vitae tincidunt tortor, at pulvinar augue.</p>
-                            </div>
-
-                        </li>
-                        <li>
-
-                            <div class="news-item-date"> <span class="news-item-day">29</span> <span class="news-item-month">Oct</span> </div>
-                            <div class="news-item-detail"> <a href="#" class="news-item-title" target="_blank">Event Name #1</a>
-                                <p class="news-item-preview">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra justo at massa suscipit, sit amet vehicula leo tristique. Phasellus vitae tincidunt tortor, at pulvinar augue.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra justo at massa suscipit, sit amet vehicula leo tristique. Phasellus vitae tincidunt tortor, at pulvinar augue.
-                                </p>
-                            </div>
-
-                        </li>
-                    </ul>
 	            </div>
 	    	</div>
 	    </div>
