aa13d23d1c9e3bbfce63b0aea5cd1b2f918e7155
Udara Karunarathna <udarapathmin@gmail.com>
2015-04-19 12:31:54 +0530

    Calendar implementation

--- application/views/year/view_year.php
+++ application/views/year/view_year.php
@@ -107,13 +107,127 @@
                                     $final[$key] = $value;
                                 });
                                 // print_r($final);
-                                foreach ($final as $key => $value) {
-                                    echo "Key: $key; Value: $value";
-                                    echo "<br />";
-                                }  
+                                // foreach ($final as $key => $value) {
+                                //     echo "Key: $key; Value: $value";
+                                //     echo "<br />";
+                                // }  
                             ?>
 
                         </div>   
+                    </div>
+                    <div class="row" style="margin-bottom:5px;">
+                        <div align="center">
+                            <?php
+                                $months=array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
+                                $current_month=date('n');
+                                $current_year=date('Y');
+                                $current_day=date('d');
+                                $month=0;
+
+                                echo '<table class="calendar">';
+                                echo '<th colspan="4" class="year">'.$current_year.'</th>';
+
+                                // Table of months
+                                for ($row=1; $row<=3; $row++) {
+                                    echo '<tr>';
+                                    for ($column=1; $column<=4; $column++) {
+                                        echo '<td class="month">';
+
+                                        $month++;
+
+                                        // Month Cell
+                                        $first_day_in_month=date('w',mktime(0,0,0,$month,1,$current_year));
+                                        $month_days=date('t',mktime(0,0,0,$month,1,$current_year));
+                                                
+                                        // in PHP, Sunday is the first day in the week with number zero (0)
+                                        // to make our calendar works we will change this to (7)
+                                        if ($first_day_in_month==0){
+                                             $first_day_in_month=7;
+                                        }
+
+                                        echo '<table>';
+                                        echo '<th colspan="7">'.$months[$month-1].'</th>';
+                                        echo '<tr class="days"><td>Mo|</td><td>Tu|</td><td>We|</td><td>Th|</td><td>Fr|</td>';
+                                        echo '<td class="sat">Sa|</td><td class="sun">Su</td></tr>';
+                                        echo '<tr>';
+
+                                        for($i=1; $i<$first_day_in_month; $i++) {
+                                            echo '<td> </td>';
+                                        }
+
+                                         for($day=1; $day<=$month_days; $day++) {
+                                                    $pos=($day+$first_day_in_month-1)%7;
+                                                    $class = (($day==$current_day) && ($month==$current_month)) ? 'today' : 'day';
+                                                    $class .= ($pos==6) ? ' sat' : '';
+                                                    $class .= ($pos==0) ? ' sun' : '';
+
+                                                    echo '<td class="'.$class.'">'.$day.'</td>';
+                                                    if ($pos==0) echo '</tr><tr>';
+                                                }
+                                                
+                                        echo '</tr>';
+                                                echo '</table>';                                                                                                       
+
+                                        echo '</td>';
+                                    }
+                                    echo '</tr>';
+                                }                                
+
+                                echo '</table>';
+                                
+                            ?>
+                        </div>
+                        <style type="text/css">
+                            .calendar {
+                                width: 800px;
+                            }
+                            .calendar, .calendar table {
+                                border: 0;
+                                margin: 0;
+                            }
+                            .calendar, .calendar table, .calendar td {
+                                text-align: center;
+                            }
+                            .calendar .year{
+                                font-family:Verdana; 
+                                font-size:18pt; 
+                                color:#ff9900;
+                            }
+                            .calendar .month{
+                                width: 25%;
+                                vertical-align: top;
+                            }
+                            .calendar .month table{
+                                font-size:9pt;
+                                font-family:Verdana;
+                            }
+                            .calendar .month th{
+                                text-align: center;
+                                font-size:12pt;
+                                font-family:Arial;
+                                color:#666699;
+                            }
+                            .calendar .month td{
+                                font-size:9pt;
+                                font-family:Verdana;
+                            }
+                            .calendar .month .days td{
+                                color:#666666;
+                                font-weight: bold;
+                            }
+                            .calendar .month .sat{
+                                color:#0000cc;
+                            }
+                            .calendar .month .sun{
+                                color:#0000cc;
+                                /*color:#cc0000;*/
+                            }
+                            .calendar .month .today{
+                                background:#ff0000;
+                                color: #ffffff;
+                            }
+
+                        </style>
                     </div>
 
                     <?php } ?>
