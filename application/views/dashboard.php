9b26abeeb3ac1cade41cedab214beef7730032fa
Udara Karunarathna <udarapathmin@gmail.com>
2015-07-12 17:51:21 +0530

    dashbord event list display

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -79,30 +79,66 @@
 	            <div class="panel-body">
                     <ul class="news-items">
                         <li>
+                            <?php foreach ($eventslist as $row) {?>
+                            <div class="news-item-date"> <span class="news-item-day">
+                                <?php 
+                                    $day=$row->start_date; 
+                                    $get_date = explode("-",$day); 
+                                    echo $get_date[2];
+                                ?>
+                                </span> <span class="news-item-month">
+                                <?php 
+                                    if($get_date[1] == 1){
+                                        echo 'Jan';
+                                    }
+                                    else if($get_date[1] == 2){
+                                        echo 'Feb';
+                                    }
+                                    else if($get_date[1] == 3){
+                                        echo 'Mar';
+                                    }
+                                    else if($get_date[1] == 4){
+                                        echo 'Apr';
+                                    }
+                                    else if($get_date[1] == 5){
+                                        echo 'may';
+                                    }
+                                    else if($get_date[1] == 6){
+                                        echo 'Jun';
+                                    }
+                                    else if($get_date[1] == 7){
+                                        echo 'Jul';
+                                    }
+                                    else if($get_date[1] == 8){
+                                        echo 'Aug';
+                                    }
+                                    else if($get_date[1] == 9){
+                                        echo 'Sep';
+                                    }
+                                    else if($get_date[1] == 10){
+                                        echo 'Oct';
+                                    }
+                                    else if($get_date[1] == 11){
+                                        echo 'Nov';
+                                    }
+                                    else{
+                                        echo 'Dec';
+                                    }
+                                ?>
+                                </span> </div>
+                            <div class="news-item-detail"> <a href="<?php echo base_url("index.php/event/view_upcoming_event_details")."/".$row->id; ?>" class="news-item-title" target="_blank"><?php echo $row->title; ?></a>
+                                <p class="news-item-preview"><?php echo $row->description; ?></p>
+                            </div>
+                            <?php }?>
+                        </li>
+                        <!-- <li>
 
-                            <div class="news-item-date"> <span class="news-item-day">29</span> <span class="news-item-month">Aug</span> </div>
-                            <div class="news-item-detail"> <a href="#" class="news-item-title" target="_blank">Event Name #1</a>
-                                <p class="news-item-preview">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra justo atLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra justo at massa suscipit, sit amet vehicula leo tristique. Phasellus vitae tincidunt tortor, at pulvinar augue.</p>
+                            <div class="news-item-date"> <span class="news-item-day"></span> <span class="news-item-month"></span> </div>
+                            <div class="news-item-detail"> <i class="news-item-title" target="_blank">No more events</i>
+                                <p class="news-item-preview"></p>
                             </div>
 
-                        </li>
-                        <li>
-
-                            <div class="news-item-date"> <span class="news-item-day">15</span> <span class="news-item-month">Jun</span> </div>
-                            <div class="news-item-detail"> <a href="#" class="news-item-title" target="_blank">Event Name #1</a>
-                                <p class="news-item-preview">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra justo at massa suscipit, sit amet vehicula leo tristique. Phasellus vitae tincidunt tortor, at pulvinar augue.</p>
-                            </div>
-
-                        </li>
-                        <li>
-
-                            <div class="news-item-date"> <span class="news-item-day">29</span> <span class="news-item-month">Oct</span> </div>
-                            <div class="news-item-detail"> <a href="#" class="news-item-title" target="_blank">Event Name #1</a>
-                                <p class="news-item-preview">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra justo at massa suscipit, sit amet vehicula leo tristique. Phasellus vitae tincidunt tortor, at pulvinar augue.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra justo at massa suscipit, sit amet vehicula leo tristique. Phasellus vitae tincidunt tortor, at pulvinar augue.
-                                </p>
-                            </div>
-
-                        </li>
+                        </li> -->
                     </ul>
 	            </div>
 	    	</div>
