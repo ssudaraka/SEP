020a75265ed3e0db3c647346a7159f3796bafe5f
Supun Sudaraka <ssudaraka@gmail.com>
2015-10-11 18:26:12 +0530

    commit before merge

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/sepclone/sep/';
+$config['base_url']	= 'http://localhost/sep/';
 
 /*
 |--------------------------------------------------------------------------
--- application/config/database.php
+++ application/config/database.php
@@ -50,8 +50,8 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '';
-$db['default']['database'] = 'ecole_new';
+$db['default']['password'] = 'root';
+$db['default']['database'] = 'sep_db';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
