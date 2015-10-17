1dd9df07650fa65e9339d941a0f6c2b79a60a6da
Udara Karunarathna <udarapathmin@gmail.com>
2015-10-17 12:56:00 +0530

    testing gitignore

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://test/sep/';
+$config['base_url']	= 'http://localhost/sep/';
 
 /*
 |--------------------------------------------------------------------------
