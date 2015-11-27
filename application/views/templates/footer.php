b93799d3cd705a85a9eefdedb2de5eda47044b48
Udara Karunarathna <udarapathmin@gmail.com>
2015-11-27 20:46:38 +0530

    Removed footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,8 +1,9 @@
-<footer>
-    <div align="center">
-        <p  style="margin-top: 10px;">Managed by SLIIT - WE Software Engineering Group</p>
+
+<!-- <footer class="footer">
+    <div class="container">
+        <p class="text-muted">Managed by SLIIT - WE Software Engineering Group</p>
     </div>
-</footer>
+</footer> -->
 
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 <script src="<?php echo base_url("assets/js/bootstrap.min.js"); ?>"></script>
