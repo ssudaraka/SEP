b877795cf95455f8d8311dfd75da4b804c6c6bf8
Udara Karunarathna <udarapathmin@gmail.com>
2015-12-05 12:35:18 +0530

    change on sms

--- application/helpers/sms_helper.php
+++ application/helpers/sms_helper.php
@@ -2,17 +2,16 @@
 
 include 'IntelliSMS.php';
 
-//Required php.ini settings:
-// allow_url_fopen = On
-// track_errors = On
-
-
-
 function send_sms(){
+	// Initialize the SMS Library
 	$objIntelliSMS = new IntelliSMS();
 
-$objIntelliSMS->Username = 'rishysr';
-$objIntelliSMS->Password = 'x1304310';
+	// Set your username and passsword
+	$objIntelliSMS->Username = 'rishysr';
+	$objIntelliSMS->Password = 'x1304310';
+
+	//Send end SMS
     $SendStatusCollection  = $objIntelliSMS->SendMessage ( '+94716544554', 'Hello, I am here', 'ecole' );
+    // Return the status
     return $SendStatusCollection;
 }
